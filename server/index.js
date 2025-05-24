1a800250233ac99651dd6b7f3de515dde9e511b2
Aman Mondal <162113539+konlin008@users.noreply.github.com>
2025-05-24 13:11:40 +0530

    Update CORS config

--- server/index.js
+++ server/index.js
@@ -21,7 +21,12 @@ app.post(
 );
 
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://lms-buwm.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/v1/media", mediaRouter);
@@ -29,7 +34,6 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/purchase", purchaseRouter);
 app.use("/api/v1/progress", courseProgressRouter);
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
